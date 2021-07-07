647845b3bb85cfd5aaa575bb3f14ba5c8368946b
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-07-07 21:54:30 +0300

    Add project files.

---
+++ CountingUntilSleep/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingUntilSleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountSheeps(new bool[] { true, false, true }));
+        }
+        public static int CountSheeps(bool[] sheeps)
+        {
+            return sheeps.Count(x => x);
+        }
+        public static int CountSheeps1(bool[] sheeps)
+        {
+            int c = 0;
+            for (int i = 0; i < sheeps.Length; i++)
+            {
+                if (sheeps[i])
+                {
+                    c++;
+                }
+            }
+            return c;
+        }
+    }
+}
